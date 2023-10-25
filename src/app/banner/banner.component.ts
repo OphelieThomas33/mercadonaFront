497905e30f46928fe3b17f925a060c6ad16d1a98
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-10-25 15:57:53 +0200

    creation of home page banner

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mcd-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.css']
+})
+export class BannerComponent {
+
+}
